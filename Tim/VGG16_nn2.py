975d5f8d9227e9617203049ab908671bc05d0bf7
Tim Classen <tim.classen1@gmx.de>
2020-05-07 00:11:34 +0200

    Improved performance

--- Tim/VGG16_nn2.py
+++ Tim/VGG16_nn2.py
@@ -5,6 +5,10 @@ import torchvision.transforms as transforms
 from torchvision import datasets
 from torchvision.models import vgg16
 
+import matplotlib.pyplot as plt
+
+import ImageNet_classlabels as IN_labels
+
 import Pt_nn
 
 from skimage.transform import resize
@@ -21,28 +25,51 @@ class VGG16_CNN(nn.Module):
 
         # dissect the network in order to get access to the feature maps of
         # the convolutional layers
+        self.vgg16.features[3].register_backward_hook(self.grad_hook)
+        self.vgg16.features[3].register_forward_hook(self.feature_hook)
+
+        self.vgg16.features[8].register_backward_hook(self.grad_hook)
+        self.vgg16.features[8].register_forward_hook(self.feature_hook)
+
+        self.vgg16.features[15].register_backward_hook(self.grad_hook)
+        self.vgg16.features[15].register_forward_hook(self.feature_hook)
+
+        self.vgg16.features[22].register_backward_hook(self.grad_hook)
+        self.vgg16.features[22].register_forward_hook(self.feature_hook)
+
         self.vgg16.features[29].register_backward_hook(self.grad_hook)
         self.vgg16.features[29].register_forward_hook(self.feature_hook)
 
         # Create class variable to store the gradients
-        self.gradients = None
+        self.gradients = []
 
-        self.feature_maps = None
+        self.feature_maps = []
 
     def grad_hook(self, layer, input, output):
-        self.gradients = input[0]
+        self.gradients.append(input[0])
 
     def feature_hook(self, layer, input, output):
-        self.feature_maps = output
+        self.feature_maps.append(output)
 
     def get_feature_gradients(self, feature_layer=None):
-        if feature_layer is None:
-            return self.gradients
+        grad_indx = 0
+        if feature_layer is not None:
+            grad_indx = feature_layer
+        return self.gradients[grad_indx]
 
     def get_feature_maps(self, x, feature_layer=None):
-        if feature_layer is None:
-            x = self.feature_maps
-            return x
+        feat_map_indx = len(self.feature_maps) - 1
+        if feature_layer is not None:
+            feat_map_indx = len(self.feature_maps) - feature_layer - 1
+        return self.feature_maps[feat_map_indx]
+
+    def get_num_featuremaps(self):
+        return len(self.feature_maps)
+
+    def reset(self):
+        self.zero_grad()
+        self.gradients = []
+        self.feature_maps = []
 
     def forward(self, x):
         x = self.vgg16(x)
@@ -70,48 +97,62 @@ def load_testset(batch_size=1, normalized=True, shuffle=True):
     return testloader
 
 
-def x_gen_heatmap(imgIndx=0):
-    # load the model
-    model = VGG16_CNN()
+def x_gen_heatmap(model, imgIndx=0, feature_layers=[0]):
+    tl_norm = iter(load_testset(batch_size=1, normalized=True, shuffle=False))
+    tl = iter(load_testset(batch_size=1, normalized=False, shuffle=False))
 
     # load the data for the heatmap generation
     for i in range(imgIndx+1):
-        input_image, label = iter(load_testset(shuffle=False)).next()
+        input_image, label = tl_norm.next()
+        image, label = tl.next()
 
-    images, labels, act_maps, pred_indcs = Pt_nn.gen_heatmap_grad(
-        model, input_image)
-    return images, labels, act_maps, pred_indcs.view(-1, pred_indcs.size(1))
+    heatmaps, pred_indx, pred_certainty = Pt_nn.gen_heatmap_grad(
+        model, input_image, torch.squeeze(image),
+        feature_layers=feature_layers)
+
+    return heatmaps, pred_indx, pred_certainty
 
 
-def x_plot_heatmaps():
-    images, labels, act_maps, pred_indcs = x_gen_heatmap()
+def x_plot_heatmaps(image_indcs, feature_indcs):
+    nrows = len(image_indcs)
+    ncols = len(feature_indcs)
 
-    scaled_act_maps = torch.zeros([
-        images.size()[0],       # batch dimension
-        act_maps.size()[1],     # feature map
-        images.size()[2],       # x-dim
-        images.size()[3]])      # y-dim
+    height = 6
+    width = 8
 
-    # scale the activation maps to the size of the original images
-    # and normalize the dynamic range of each image to a range of [0, 1]
-    for img_indx in range(act_maps.size()[0]):
-        for feature_indx in range(act_maps.size()[1]):
-            scaled_act_maps[img_indx, feature_indx, :, :] =\
-                torch.tensor(resize(
-                    act_maps[img_indx, feature_indx, :, :].detach().numpy(),
-                    images.size()[2:]))
+    # load the model
+    model = VGG16_CNN()
 
-    Pt_nn.plot_heatmaps(images, labels, scaled_act_maps, pred_indcs, 5)
+    fig, axs = plt.subplots(
+        nrows=nrows, ncols=ncols, figsize=[height, width])
+
+    for ax in axs.flat:
+        ax.axis('off')
+
+    for img_indx in range(len(image_indcs)):
+        heatmap, pred_indx, pred_certainty = x_gen_heatmap(
+            model, imgIndx=image_indcs[img_indx], feature_layers=feature_indcs)
+        for feature_indx in range(len(feature_indcs)):
+            x = img_indx
+            y = feature_indx
+
+            axs[x, y].imshow(heatmap[feature_indx])
+            axs[x, y].set_title(
+                "Prediction: " + IN_labels.classlabels[pred_indx.item()])
+
+    plt.show()
 
 
 if __name__ == '__main__':
-    tl = load_testset(batch_size=1, shuffle=False)
-    input_image, label = iter(tl).next()
+    # tl = load_testset(batch_size=1, shuffle=False)
+    # input_image, label = iter(tl).next()
 
-    tl = load_testset(batch_size=1, normalized=False, shuffle=False)
-    image, _ = iter(tl).next()
-    image = torch.squeeze(image)
+    # tl = load_testset(batch_size=1, normalized=False, shuffle=False)
+    # image, _ = iter(tl).next()
+    # image = torch.squeeze(image)
 
-    model = VGG16_CNN()
+    # model = VGG16_CNN()
 
-    Pt_nn.gen_heatmap_grad(model, input_image, image)
+    # Pt_nn.gen_heatmap_grad(model, input_image, image)
+
+    x_plot_heatmaps([24, 25], [0, 2, 4])
